f43daf976dab6be9bc60ec6520a00f5b24e5a8af
mijinson <103203170+mijinson@users.noreply.github.com>
2022-05-11 12:37:17 -0700

    added files

--- skilldemo.java
+++ skilldemo.java
@@ -1,1 +1,12 @@
+public class skilldemo {
+    public static boolean searchFor(int n) {
+        Integer[] myArray = {1, 2, 3};
+        for (int i = 0; i < myArray.length; i++) {
+            if (myArray[i] == n) {
+                return true;
+            }
+        }
+        return false;
 
+    }
+}
---
+++ skilldemoTester.java
@@ -0,0 +1,9 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class skilldemoTester {
+    @Test
+    public void testSearch() {
+        assertTrue(skilldemo.searchFor(4));
+    }
+}
